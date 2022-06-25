180a11b1826d38f120c5f49f296a9c58d4a2a199
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-06-25 20:01:41 +0100

    part 3 : 2:15:00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CustomersComponent } from './customers/customers.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -19,7 +20,7 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CustomerService} from "../services/customer.service";
+import {catchError, Observable, throwError} from "rxjs";
+import {Customer} from "../model/customer.model";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-customers',
@@ -8,19 +11,26 @@ import {CustomerService} from "../services/customer.service";
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
+  customers! : Observable<Array<Customer>>;
+  errorMessage! : string;
+  searchFormGroup : FormGroup | undefined;
 
-  customers: any;
-  constructor(private customerService : CustomerService) { }
+  constructor(private customerService : CustomerService, private fb : FormBuilder) { }
 
   ngOnInit(): void {
-    this.customerService.getCustomers().subscribe({
-      next : (data)=>{
-        this.customers=data;
-      },
-      error : (err)=>{
-        console.log(err);
-      }
+    this.searchFormGroup=this.fb.group({
+      keyword : this.fb.control("")
     });
+    this.handleSearchCustomers();
   }
 
+  handleSearchCustomers() {
+    let kw = this.searchFormGroup?.value.keyword;
+    this.customers=this.customerService.searchCustomers(kw).pipe(
+      catchError(err=>{
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
+  }
 }
