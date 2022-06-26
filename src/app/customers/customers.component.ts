dc173374c74e23ece84ca51c1cd5bcba28163c6e
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-06-26 04:45:32 +0100

    part 4 : complete

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -4,6 +4,7 @@ import {CustomerService} from "../services/customer.service";
 import {catchError, map, Observable, throwError} from "rxjs";
 import {Customer} from "../model/customer.model";
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-customers',
@@ -15,7 +16,7 @@ export class CustomersComponent implements OnInit {
   errorMessage! : string;
   searchFormGroup : FormGroup | undefined;
 
-  constructor(private customerService : CustomerService, private fb : FormBuilder) { }
+  constructor(private customerService : CustomerService, private fb : FormBuilder, private router : Router) { }
 
   ngOnInit(): void {
     this.searchFormGroup=this.fb.group({
@@ -51,5 +52,9 @@ export class CustomersComponent implements OnInit {
         console.log(err);
       }
     })
+  }
+
+  handleCustomerAccounts(customer: Customer) {
+    this.router.navigateByUrl("/customer-accounts/"+customer.id,{state :customer});
   }
 }
